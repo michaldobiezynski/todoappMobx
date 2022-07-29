de9ec52dfd6605bd3b4878c7789a7405fe1c2726
michaldobiezynski <michaldobiezynski@hotmail.com>
2022-07-29 20:17:25 +0100

    stop trying to send the new todo to the backend

--- src/ToDo/ToDoStore.ts
+++ src/ToDo/ToDoStore.ts
@@ -13,6 +13,7 @@ export default class ToDoStore {
     async init() {
         let response = await fetch(this.todoAPI);
         let newToDos: ToDoModel[] = await response.json();
+        console.log("newToDos", newToDos)
         this.addToDoToStore(newToDos);
     }
 
@@ -22,6 +23,7 @@ export default class ToDoStore {
         for (let todo of ToDos) {
             this.ToDos.push(todo);
         }
+        console.log("this.ToDos", this.ToDos)
     }
 
     @action.bound
@@ -30,18 +32,20 @@ export default class ToDoStore {
     }
 
     @action.bound
-    async addToDo(title: string, isCompleted: boolean) {
-        let response = await fetch(this.todoAPI, {
-            method: 'Post',
-            headers: { 'content-type': 'application/json' },
-            body: JSON.stringify({ title, isCompleted }),
-        });
-        let createdToDo = await response.json();
-        this.addNewToDoToStore(createdToDo);
+    async addToDo(title: string, completed: boolean) {
+        // let response = await fetch(this.todoAPI, {
+        //     method: 'Post',
+        //     headers: { 'content-type': 'application/json' },
+        //     body: JSON.stringify({ title, isCompleted }),
+        // });
+        // let createdToDo = await response.json();
+        this.addNewToDoToStore({ id: Math.floor(Math.random() * 10) + 1, title, completed });
     }
 
     @action.bound
     async addNewToDoToStore(todo: ToDoModel) {
+        console.log("I go here", todo)
+        console.log("bla bla", this.ToDos)
         this.ToDos.push(todo);
     }
 }
