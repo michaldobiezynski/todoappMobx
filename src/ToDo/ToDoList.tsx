2cfcb14d81bff5cf9a7bf077c9fa42fda3e1ccfb
michaldobiezynski <michaldobiezynski@hotmail.com>
2022-07-29 20:19:25 +0100

    remove colour circles from to do list

--- src/ToDo/ToDoList.tsx
+++ src/ToDo/ToDoList.tsx
@@ -12,6 +12,7 @@ export default class ToDoList extends React.Component<
     if (this.props.ToDos.length === 0) {
       return <div>Please add wishlist to get started</div>;
     }
+    console.log("am I getting re-rendered?");
 
     return (
       <div>
@@ -35,12 +36,7 @@ export default class ToDoList extends React.Component<
                       viewBox="0 0 16 16"
                       className="bi bi-check-circle-fill"
                       fill="green"
-                      xmlns="http://www.w3.org/2000/svg">
-                      <path
-                        fill-rule="evenodd"
-                        d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"
-                      />
-                    </svg>
+                      xmlns="http://www.w3.org/2000/svg"></svg>
                   ) : (
                     <svg
                       width="1.4em"
@@ -48,13 +44,7 @@ export default class ToDoList extends React.Component<
                       viewBox="0 0 16 16"
                       className="bi bi-x-circle-fill"
                       fill="#fc2222"
-                      xmlns="http://www.w3.org/2000/svg">
-                      <path
-                        fill-rule="evenodd"
-                        color="white"
-                        d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"
-                      />
-                    </svg>
+                      xmlns="http://www.w3.org/2000/svg"></svg>
                   )}
                 </div>
               </Row>
